321477e6c3c049ef9a5e97f32dd4dce798f96807
Nutnaree <nutnaree.kii@gmail.com>
2019-02-17 12:18:11 +0700

    updateForm

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { User } from '../User';
 
 @Component({
   selector: 'app-form',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  // formBuild:FormBuilder; //ภายนอกใช้ชื่อเต็ม ภายในใช้ชื่อย่อ
+  formGroup: FormGroup;
+  constructor( // ของใน constructor ไม่สามารถนำออกมาข้างนอกได้
+    private formBuild: FormBuilder
+  ) { 
+    // this.formBuild = fb; // สร้างชื่อขึ้นมาเพื่อเอาไว้เก็บค่า
   }
 
+  ngOnInit() {
+    this.formGroup = this.formBuild.group({
+      firstName: this.formBuild.control('Nutnaree'),
+      lastName: ['Kitprasertpol'],
+      email:['nutnaree.ki@ku.th'],
+      age:['22']
+    })
+  }
+
+  onSubmit(form: FormGroup){
+    const {firstName, lastName, email, age} = form.value;
+    const user = new User(firstName, lastName, email, age);
+    console.log(user);
+  }
 }
